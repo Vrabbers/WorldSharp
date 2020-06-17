232311ef5ce52644edba2afdd190c85e5a4c8860
Vrabbers <nininininimocp@gmail.com>
2020-06-17 17:36:44 -0300

    consts, NuttalWindow(), DCCorrection() and GetSuitableFFTSize()

--- WorldSharp/Common.cs
+++ WorldSharp/Common.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace WorldSharp
 {
@@ -9,37 +10,67 @@ namespace WorldSharp
         /// </summary>
         /// <param name="sample">Length of the input signal.</param>
         /// <returns>Suitable FFT size</returns>
-        public static int GetSuitableFftSize(int sample) { }
-        
+        public static int GetSuitableFftSize(int sample) =>
+            (int) Math.Pow(2, (int) Math.Log(sample) / Consts.KLog2 + 1);
+
         //My(Max|Min)(Int|Double)() replaced with standard Math.Max/Min()
-        
-        
+
+
         // docs paraphrased from common.h
         /// <summary>
         /// DCCorrection interpolates the power under f0 Hz and is used in CheapTrick() and D4C().
         /// </summary>
-        public static double[] DCCorrection(double[] input, double currentF0, int fs, int fftSize) { }
-        
+        public static double[] DCCorrection(double[] input, double f0, int fs, int fftSize)
+        {
+            var upperLimit = 2 + (int) (f0 * fftSize / fs);
+            var lowFreqAxis = new double[upperLimit];
+
+            for (var i = 0; i < upperLimit; i++)
+                lowFreqAxis[i] = ((double) i) * fs / fftSize;
+
+            var upperLimitReplica = upperLimit - 1;
+            var lowFreqReplica = Matlab.Interpolate1Q(f0 - lowFreqAxis[0], -(double) fs / fftSize,
+                input[..(upperLimit + 1)], lowFreqAxis[..upperLimitReplica]);
+
+            var output = new double[upperLimitReplica];
+            for (var i = 0; i < upperLimitReplica; i++)
+                output[i] = input[i] + lowFreqReplica[i];
+            return output;
+        }
+
         /// <summary>
         /// LinearSmoothing() carries out the spectral smoothing by rectangular window whose length is
         /// <paramref name="width"/> Hz and is used in CheapTrick() and D4C().
         /// </summary>
-        public static double[] LinearSmoothing(double[] input, double width, int fs, int fft_size) { }
-        
-        
+        public static double[] LinearSmoothing(double[] input, double width, int fs, int fft_size)
+        {
+        }
+
+
         /// <summary>
         /// NuttallWindow() calculates the coefficients of Nuttall window whose length is <paramref name="yLength"/>
         /// and is used in Dio(), Harvest() and D4C().
         /// </summary>
-        public static double[] NuttalWindow(int yLength) { }
+        public static double[] NuttalWindow(int yLength)
+        {
+            var y = new double[yLength];
+            for (var i = 0; i < yLength; i++)
+            {
+                var tmp = i / (yLength - 1.0);
+                y[i] = 0.355768 - 0.487396 * Math.Cos(2.0 * Consts.KPi * tmp) +
+                       0.144232 * Math.Cos(4.0 * Consts.KPi * tmp) - 0.012604 * Math.Cos(6.0 * Consts.KPi * tmp);
+            }
+
+            return y;
+        }
 
 
         /// <summary>
         /// GetSafeAperiodicity() limits the range of aperiodicity from 0.001 to 0.999999999999 (1 - world::kMySafeGuardMinimum).
         /// </summary>
         //[[MethodImpl(MethodImplOptions.AggressiveInlining)] :when:
-        public static double GetSafeAperiodicity(double x) => Math.Clamp(x, 0.001, 0.999999999999); //TODO: some nicer way?
-        
+        public static double GetSafeAperiodicity(double x) => Math.Clamp(x, 0.001, 1 - Consts.KMySafeGuardMinimum);
+
         //Initialize[SomeSortOfFFT]() functions will be implemented as constructors in their respective classes.
     }
 }
---
+++ WorldSharp/Consts.cs
@@ -0,0 +1,39 @@
+﻿namespace WorldSharp
+{
+    public static class Consts
+    {
+        // constantnumber.h consts
+        public const double KCutOff = 50.0;
+
+        // for StoneMask()
+        public const double KFloorF0StoneMask = 40.0;
+
+        public const double KPi = 3.1415926535897932384;
+        public const double KMySafeGuardMinimum = 0.000000000001;
+        public const double KEps = 0.00000000000000022204460492503131;
+        public const double KFloorF0 = 71.0;
+        public const double KCeilF0 = 800.0;
+        public const double KDefaultF0 = 500.0;
+        public const double KLog2 = 0.69314718055994529;
+
+        // Maximum standard deviation not to be selected as a best f0.
+        public const double KMaximumValue = 100000.0;
+
+        // for D4C()
+        public const int KHanning = 1;
+        public const int KBlackman = 2;
+        public const double KFrequencyInterval = 3000.0;
+        public const double KUpperLimit = 15000.0;
+        public const double KThreshold = 0.85;
+        public const double KFloorF0D4C = 47.0;
+
+        // for Codec (Mel scale)
+        // S. Stevens & J. Volkmann,
+        // The Relation of Pitch to Frequency: A Revised Scale,
+        // American Journal of Psychology, vol. 53, no. 3, pp. 329-353, 1940.
+        public const double KM0 = 1127.01048;
+        public const double KF0 = 700.0;
+        public const double KFloorFrequency = 40.0;
+        public const double KCeilFrequency = 20000.0;
+    }
+}
