451d95dedb2757744e928c56a1610fa8cd474f7a
Vrabbers <nininininimocp@gmail.com>
2020-06-19 03:02:49 -0300

    LinearSmoothing

--- WorldSharp/Common.cs
+++ WorldSharp/Common.cs
@@ -42,8 +42,51 @@ namespace WorldSharp
         /// LinearSmoothing() carries out the spectral smoothing by rectangular window whose length is
         /// <paramref name="width"/> Hz and is used in CheapTrick() and D4C().
         /// </summary>
-        public static double[] LinearSmoothing(double[] input, double width, int fs, int fft_size)
+        public static double[] LinearSmoothing(double[] input, double width, int fs, int fftSize)
         {
+            var boundary = (int) (width * fftSize / fs) + 1;
+            var mirroringSpectrum = new double[fftSize / 2 + boundary * 2 + 1];
+            var mirroringSegment = new double[fftSize / 2 + boundary * 2 + 1];
+            var frequencyAxis = new double[fftSize / 2 + 1];
+
+            SetParametersForLinearSmoothing(boundary, fftSize, fs, width, input, ref mirroringSpectrum,
+                ref mirroringSegment, ref frequencyAxis);
+
+            var originOfMirroringAxis = -(boundary - 0.5) * fs / fftSize;
+            var discreteFreqInterval = (double) fs / fftSize;
+
+            var lowLevels = Matlab.Interpolate1Q(originOfMirroringAxis, discreteFreqInterval,
+                mirroringSegment, frequencyAxis);
+
+            for (var i = 0; i <= fftSize / 2; i++) frequencyAxis[i] += width;
+
+            var highLevels = Matlab.Interpolate1Q(originOfMirroringAxis, discreteFreqInterval,
+                mirroringSegment, frequencyAxis);
+
+            var output = new double[fftSize / 2 + 1];
+            for (var i = 0; i < fftSize / 2 + 1; i++) output[i] = (highLevels[i] - lowLevels[i]) / width;
+
+            return output;
+        }
+
+
+        //TODO: Inline this because it's pretty silly
+        static void SetParametersForLinearSmoothing(int boundary, int fftSize, int fs, double width,
+            double[] powerSpectrum, ref double[] mirroringSpectrum, ref double[] mirroringSegment,
+            ref double[] frequencyAxis)
+        {
+            for (var i = 0; i < boundary; i++)
+                mirroringSpectrum[i] = powerSpectrum[boundary - i];
+            for (var i = boundary; i < fftSize / 2 + boundary; i++)
+                mirroringSpectrum[i] = powerSpectrum[i - boundary];
+            for (var i = fftSize / 2 + boundary; i <= fftSize / 2 + boundary * 2; i++)
+                mirroringSpectrum[i] = powerSpectrum[fftSize / 2 - (i - (fftSize / 2 + boundary))];
+
+            mirroringSegment[0] = mirroringSpectrum[0] * fs / fftSize;
+            for (var i = 1; i < fftSize / 2 + boundary * 2 + 1; i++)
+                mirroringSegment[i] = mirroringSpectrum[i] * fs / fftSize + mirroringSegment[i - 1];
+
+            for (var i = 0; i <= fftSize / 2; i++) frequencyAxis[i] = (double) i / fftSize * fs - width / 2.0;
         }
 
 
@@ -58,7 +101,7 @@ namespace WorldSharp
             {
                 var tmp = i / (yLength - 1.0);
                 y[i] = 0.355768 - 0.487396 * Math.Cos(2.0 * Consts.KPi * tmp) +
-                       0.144232 * Math.Cos(4.0 * Consts.KPi * tmp) - 0.012604 * Math.Cos(6.0 * Consts.KPi * tmp);
+                    0.144232 * Math.Cos(4.0 * Consts.KPi * tmp) - 0.012604 * Math.Cos(6.0 * Consts.KPi * tmp);
             }
 
             return y;
