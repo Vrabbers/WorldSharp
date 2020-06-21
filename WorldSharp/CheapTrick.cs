6e2dc225a463b3949663e1bbcd3fccfca2699c18
Vrabbers <nininininimocp@gmail.com>
2020-06-21 16:02:15 -0300

    Create CheapTrick.cs

---
+++ WorldSharp/CheapTrick.cs
@@ -0,0 +1,14 @@
+﻿namespace WorldSharp
+{
+    public static class CheapTrick
+    {
+        
+    }
+
+    struct CheapTrickOptions
+    {
+        double q1;
+        double f0Floor; 
+        int fftSize;
+    }
+}
